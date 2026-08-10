a2bc3d794ed750cee4e53216ce7500cbfb705e67
SheavyaSree Kotra <sheavyasreek@gmail.com>
2026-08-10 12:05:08 +0530

    Time: 4 ms (81.14%), Space: 43.5 MB (30.51%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+       int[] a = (int[]) heights.clone();
+       Arrays.sort(a);
+       int c = 0;
+       for(int i =0;i<a.length;i++)
+       {
+        if(a[i] != heights[i])
+        {
+            c++;
+        }
+       } 
+       return c;
+    }
+}
